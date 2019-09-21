ddf1d03b6449f844e9b2579843e9bc1b3fd051a3
Adriano-97 <adriano199726@gmail.com>
2019-09-21 05:32:50 -0400

    This funcion will send messages to the user.

---
+++ resources/views/inc/messages.blade.php
@@ -0,0 +1,24 @@
+@if(count($errors) > 0))
+    @foreach($errors->all() as $error)
+        <div class="alert alert-danger">
+            {{$error}}
+        </div>
+    @endforeach
+@endif
+@if ($message = Session::get('error'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+ @endif
+
+@if(session('success'))
+    <div class="alert alert-success">
+        {{session('success')}}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{session('error')}}
+    </div>
+@endif
