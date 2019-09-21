c0f4eba07f26c3dabea0928e0bf277b768e4a4c7
Adriano-97 <adriano199726@gmail.com>
2019-09-21 03:41:25 -0400

    Updated the Post controller so it could handle index and Show requests.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostsController extends Controller
 {
     /**
@@ -13,7 +13,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        $post = Post::all();
+        return view('posts.index')->with('posts', $post);
     }
 
     /**
@@ -45,7 +46,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view(posts.show)->with('post',$post);
     }
 
     /**
