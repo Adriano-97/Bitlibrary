52a64f5f980ca8278b422ccd98abdf0d60849b29
Adriano-97 <adriano199726@gmail.com>
2019-09-22 07:48:56 -0400

    Added support for pictures.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,15 +3,22 @@
 @section('content')
 
     <h1>Post</h3>
-    @if(count($posts) > 1)
+    @if(count($posts) > 0)
     @foreach ($posts as $post)
-        <div class="card card-body bg-light">
-            <h3>
-            <a href="posts/{{$post->id}}"> {{$post->title}} </a>
-            </h3>
-            <small>
-                {{$post->created_at}}
-            </small>
+        <div class="well">
+            <div class="row">
+                <div class = "col-md-3 col-sm-3">
+                   <img style = "width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+                </div>
+                <div class = "col-md-8 col-sm-8">
+                    <h3>
+                        <a href="posts/{{$post->id}}"> {{$post->title}} </a>
+                    </h3>
+                    <small>
+                        {{$post->created_at}} by {{$post->user->name}}
+                    </small>
+                </div>
+            </div>
         </div>
     @endforeach
     @else
