5f93d1d6e350a2e9741c603305b7f3e5e7334c11
Adriano-97 <adriano199726@gmail.com>
2019-10-02 15:15:16 -0400

    Restructure the index pahe for library.

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -1,12 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
-        <h1>Test Index</h1>
-        <p>Lorem ipsum balalalalalala</p>
-        <p>
-            <a class ="btn btn-primary btn-lg" href="/login" role="button">Loggin</a>
-            <a class ="btn btn-success btn-lg" href="/register" role="button">Register</a>
-        </p>
-    </div>
+    <h1>Library</h1>
+    @if (count($books) > 0)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Book Name</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book)
+                    <tr>
+                        <td>{{$book->title}}</td>
+                        <td><a href="/library/{{$book->id}}" class="btn btn-default">More details</a></td>
+                        <td><a href="" class="btn btn-default">Download</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+        </table>
+    @else
+        <p>No Books found</p>
+    @endif
+
 @endsection
