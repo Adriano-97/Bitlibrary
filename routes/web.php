d3f6f78dfdca17f89feb3753ab1c2a71b4270ef9
Adriano-97 <adriano199726@gmail.com>
2019-10-02 23:33:51 -0400

    added the download route.

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ Route::get('/', 'PagesController@index');
 Route::get('/pages/about', 'PagesController@about');
 Route::get('/pages/services', 'PagesController@services');
 Route::get('/dashboard', 'DashboardController@index');
-
+Route::get('/download/{id}', 'LibraryController@download');
 Route::resource('posts', 'PostsController');
 Route::resource('library', 'LibraryController');
-
 
 Auth::routes();
