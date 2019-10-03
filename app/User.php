585e7e46cfeb0770cccfb5e96cd224faa2bc62e8
Adriano-97 <adriano199726@gmail.com>
2019-10-03 02:05:50 -0400

    Added the new files to EC2

--- app/User.php
+++ app/User.php
@@ -40,4 +40,7 @@ class User extends Authenticatable
     public function posts(){
         return $this->hasMany('App\Post');
     }
+    public function book(){
+        return $this->hasMany('App\Book');
+    }
 }
---
+++ resources/views/library/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Edit Book</h1>
+    {!! Form::open(['action' => ['LibraryController@update', $books->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', $books->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body', 'Description')}}
+            {{Form::textarea('body', $books->description, ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Short Description'])}}
+        </div>
+        <div class = "form-goup">
+            {{Form::file('pdf_file')}}
+        </div>
+        {{Form::hidden('_method', 'PATCH')}}
+        <br><br>
+        {{ Form::submit('Submit', ['class' => 'btn btn-primary float-right']) }}
+        {!! Form::close() !!}
+         {{-- Delete BTN --}}
+        {!! Form::open(['action' => ['LibraryController@destroy', $books->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {!! Form::submit("Delete", ['class'=>'btn btn-danger float-left']) !!}
+        {!! Form::close() !!}
+
+@endsection
