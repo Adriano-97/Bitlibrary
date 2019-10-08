b060dcf0cc3b92c62ff8afa9108f26d3d409507b
Adriano-97 <adriano199726@gmail.com>
2019-10-08 18:12:10 -0400

    Restructured the layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,5 @@
-<!doctype html>
+{{-- Old CSS Styles --}}
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -17,8 +18,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+{{-- </head> --}}
+
 <body>
 
     <div id="app">
@@ -35,4 +37,4 @@
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
 </body>
-</html>
+{{-- </html> --}}
