93a8c9122c8ef4bbc1f7d77bc9c1f4afa45d9381
Adriano-97 <adriano199726@gmail.com>
2019-10-09 00:47:12 -0400

    Restiled the welcome page.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,22 @@
+<!DOCTYPE html>
+<html>
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
-        <h1>Test Index</h1>
-        <p>Lorem ipsum balalalalalala</p>
-        <p>
-            <a class ="btn btn-primary btn-lg" href="/login" role="button">Loggin</a>
-            <a class ="btn btn-success btn-lg" href="/register" role="button">Register</a>
-        </p>
+    <div class="jumbotron" id="homeJumbo" style="background-image: url(&quot;/img/homeBackground.jpg&quot;);background-size: cover;opacity: 1;">
+        <div>
+            <div>
+                <h1 class="text-center" id="h1id" style="font-family: 'Alegreya Sans', sans-serif;background-color: rgba(21,162,184,0.62);">Welcome To BitLibrary</h1>
+            </div>
+            <p class="text-center" id="parid" style="background-color: rgba(70,187,205,0.72);">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                <br><br><br>
+            </p>
+            <br><br>
+            <div id="btnDiv">
+                <a class="btn btn-primary" href="/login" role="button" id="login">Login</a>
+                <a class="btn btn-success" href="/register" role="button" id="register">Register</a>
+            </div>
+        </div>
     </div>
+
 @endsection
