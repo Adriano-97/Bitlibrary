5fa5b1f7115f51a7633483cd3530e6773c1bb4b0
Adriano-97 <adriano199726@gmail.com>
2019-10-10 18:05:54 -0400

    Restructured the about page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,43 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+            <link rel="stylesheet" href="/css/bootstrap/css/bootstrap2.min.css">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+            <link rel="stylesheet" href="/css/fonts/font-awesome.min.css">
+            <link rel="stylesheet" href="/css/css/untitled.css">
+    </head>
+    @include('inc.navbar')
+<body>
+    <header class="masthead" style="background-image: url(&quot;/img/homeBackground.jpg&quot;);">
+        <div class="overlay"></div>
+        <div class="container" id="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="site-heading">
+                        <h1 id="noH1">About Me</h1>
+                        <img src="/img/profile.jpg"id ="profile"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container" id="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 mx-auto">
+                <br>
+                <br>
+                <p>My name is Adriano Herrera, I'm a CS major currently studying at FIU. I created this template of a Q&A website to showcase my abilities as a Full-Stack web developer.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="footer-basic">
+            <footer id="myFooter">
+                <p id="footer" class="copyright" style="opacity: 1;">Bitlord &nbsp;2019</p>
+            </footer>
+    </div>
+</body>
+</html>
 
-@section('content')
-
-    <h1>Test about</h1>
-    <p>Lorem ipsum balalalalalala</p>
-
-@endsection
